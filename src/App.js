4b7af8a5dc51a5112d989650e0e2adc2aa06c741
Karolina Obcowska <jimmy.obcowska@gmail.com>
2022-01-31 16:51:45 +0100

    conversations list created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
+/* eslint-disable no-unused-vars */
 import Login from './components/Login';
+import Conversations from './components/Conversations';
 
 /* eslint-disable react/react-in-jsx-scope */
 function App() {
   return (
     <div className="App">
-      <Login />
+      {/* <Login /> */}
+      <Conversations />
     </div>
   );
 }
---
+++ src/components/Conversations.js
@@ -0,0 +1,82 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Wrapper = styled.section`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+  width: 30%;
+`;
+
+const Title = styled.h2`
+  color: white;
+  margin-top: 2rem;
+  font-weight: 1;
+  font-size: 1.2rem;
+`;
+
+const List = styled.ul`
+  margin-top: 1rem;
+  list-style: none;
+  width: 100%;
+`;
+
+const ListItem = styled.li`
+  width: 100%;
+  height: 5rem;
+  color: white;
+  margin-top: 1rem;
+  background: rgba(255, 255, 255, 0.2);
+  backdrop-filter: blur(7.5px);
+  -webkit-backdrop-filter: blur(7.5px);
+  border: 1px solid rgba(255, 255, 255, 0.18);
+  border-radius: 10px;
+  padding: 1rem;
+  font-weight: 1;
+`;
+
+const ItemDetail = styled.p`
+  color: white;
+  font-size: 1rem;
+`;
+
+const LastMessage = styled.small`
+  font-size: 0.6rem;
+`;
+
+const Conversations = () => {
+  return (
+    <Wrapper>
+      <Title>Conversations</Title>
+      <List>
+        <ListItem>
+          <ItemDetail>Karolina Obcowska</ItemDetail>
+          <LastMessage>Blablabla</LastMessage>
+        </ListItem>
+        <ListItem>
+          <ItemDetail>Karolina Obcowska</ItemDetail>
+          <LastMessage>Blablabla</LastMessage>
+        </ListItem>
+        <ListItem>
+          <ItemDetail>Karolina Obcowska</ItemDetail>
+          <LastMessage>Blablabla</LastMessage>
+        </ListItem>
+        <ListItem>
+          <ItemDetail>Karolina Obcowska</ItemDetail>
+          <LastMessage>Blablabla</LastMessage>
+        </ListItem>
+        <ListItem>
+          <ItemDetail>Karolina Obcowska</ItemDetail>
+          <LastMessage>Blablabla</LastMessage>
+        </ListItem>
+        <ListItem>
+          <ItemDetail>Karolina Obcowska</ItemDetail>
+          <LastMessage>Blablabla</LastMessage>
+        </ListItem>
+      </List>
+    </Wrapper>
+  );
+};
+
+export default Conversations;
